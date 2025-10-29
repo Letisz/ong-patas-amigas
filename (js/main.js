c7ce718ac82eb7234ff21f79351082727b3fc7db
Leticia Araújo <le1771351@gmail.com>
2025-10-29 04:45:52 -0300

    Add volunteer alert on button click

---
+++ (js/main.js
@@ -0,0 +1,7 @@
+// Exemplo: alerta para voluntariado
+document.addEventListener("DOMContentLoaded", () => {
+    const btnVoluntario = document.querySelector(".btn");
+    btnVoluntario.addEventListener("click", () => {
+        alert("Obrigado por se interessar em ser voluntário da Patas Amigas!");
+    });
+});
